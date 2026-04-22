18c3c8faa8cf9eade754cc98a0c40d199a2151f5
ahad-m <ahad.malotaibii@gmail.com>
2026-04-22 04:52:26 +0300

    edit  memory game

--- src/pages/PoetryMemoryGame.tsx
+++ src/pages/PoetryMemoryGame.tsx
@@ -23,6 +23,7 @@ const MEMORIZE_SECONDS = 5;
 function normalizeForCompare(text: string): string {
   return (text || "")
     .replace(/[\u0610-\u061A\u064B-\u065F\u0670\u06D6-\u06ED]/g, "")
+    .replace(/ـ/g, "")  // ← أضف هذا السطر لإزالة التطويل
     .replace(/[إأآٱ]/g, "ا")
     .replace(/ة/g, "ه")
     .replace(/ى/g, "ي")
